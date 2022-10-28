5315c64c93fbcee2f4a3e25f2bf2b8ade14b941e
wujingling <claire0821@126.com>
2022-10-28 11:36:37 +0800

    移除腾讯云短信okhttp版本与springboot2.7冲突

--- like-admin/src/main/java/com/mdd/admin/LikeAdminApplication.java
+++ like-admin/src/main/java/com/mdd/admin/LikeAdminApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * 启动器
  */
+@EnableDiscoveryClient
 @Configuration
 @ComponentScan(basePackages = {"com.mdd"})
 @MapperScan(basePackages = {"com.mdd.*.mapper"})
--- like-common/src/main/java/com/mdd/common/plugin/sms/engine/TencentSms.java
+++ like-common/src/main/java/com/mdd/common/plugin/sms/engine/TencentSms.java
@@ -3,9 +3,9 @@ package com.mdd.common.plugin.sms.engine;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.profile.ClientProfile;
 import com.tencentcloudapi.common.profile.HttpProfile;
-import com.tencentcloudapi.sms.v20210111.SmsClient;
-import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest;
-import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
+//import com.tencentcloudapi.sms.v20210111.SmsClient;
+//import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest;
+//import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -93,30 +93,31 @@ public class TencentSms {
             clientProfile.setSignMethod("HmacSHA256");
             clientProfile.setHttpProfile(httpProfile);
 
-            /*参数配置*/
-            SmsClient client = new SmsClient(cred, "ap-guangzhou",clientProfile);
-            SendSmsRequest req = new SendSmsRequest();
-            req.setSignName(config.get("sign"));
-            req.setSmsSdkAppId(config.get("appId"));
-            req.setTemplateId(this.templateId);
-
-            /*手机号码*/
-            String[] phoneNumberSet = {"+86"+this.mobile};
-            req.setPhoneNumberSet(phoneNumberSet);
-
-            /*模板参数*/
-            String[] templateParamSet = this.templateParams;
-            req.setTemplateParamSet(templateParamSet);
-
-            /*发起请求*/
-            SendSmsResponse res = client.SendSms(req);
-            if (!res.getSendStatusSet()[0].getCode().equals("Ok")) {
-                this.sendResult = 2;
-                return res.getSendStatusSet()[0].getMessage();
-            }
-
-            this.sendResult = 1;
-            return res.getSendStatusSet()[0].getMessage();
+//            /*参数配置*/
+//            SmsClient client = new SmsClient(cred, "ap-guangzhou",clientProfile);
+//            SendSmsRequest req = new SendSmsRequest();
+//            req.setSignName(config.get("sign"));
+//            req.setSmsSdkAppId(config.get("appId"));
+//            req.setTemplateId(this.templateId);
+//
+//            /*手机号码*/
+//            String[] phoneNumberSet = {"+86"+this.mobile};
+//            req.setPhoneNumberSet(phoneNumberSet);
+//
+//            /*模板参数*/
+//            String[] templateParamSet = this.templateParams;
+//            req.setTemplateParamSet(templateParamSet);
+//
+//            /*发起请求*/
+//            SendSmsResponse res = client.SendSms(req);
+//            if (!res.getSendStatusSet()[0].getCode().equals("Ok")) {
+//                this.sendResult = 2;
+//                return res.getSendStatusSet()[0].getMessage();
+//            }
+//
+//            this.sendResult = 1;
+//            return res.getSendStatusSet()[0].getMessage();
+            return "";
         } catch (Exception e) {
             this.sendResult = 2;
             return e.getMessage();
